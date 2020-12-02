f9518c5c2bdd048a0aa42ca6acc50353664e0a10
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-02 22:35:34 +0900

    ユーザー管理機能の実装のブランチ違い実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, null: false
-  validates :email, null: false, unique: true
-  validates :last_name,
-  validates :last_name_kana,
-  validates :first_name,
-  validates :first_name_kana,
-  validates :birthday
+  with_options null: false do
+    validates :nickname, format: { with: /\A[ぁ-ん一-龥]/, message:"は全角で入力してください。"}
+    validates :email, unique: true
+    validates :last_name,A[ぁ-ん一-龥],message:'全角文字を使用してください'
+    validates :last_name_kana
+    validates :first_name
+    validates :first_name_kana
+    validates :birthday
+  end
 end
