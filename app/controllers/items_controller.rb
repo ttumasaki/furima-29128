efed64ec8db3f71daed01ed0257b2ec08c2edee0
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-22 23:32:28 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :items_params
+
   def index
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:image).merge(:user_id)
   end
 end
