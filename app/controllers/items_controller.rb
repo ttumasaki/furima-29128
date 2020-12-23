0827e04f0df0e38f056938321ee51b62027eb305
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-23 21:45:19 +0900

    ブランチの差異削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,4 @@
 class ItemsController < ApplicationController
-<<<<<<< Updated upstream
-  def index
-  end
-=======
-
 
   def index
   end
@@ -12,12 +7,10 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
   end
 
   private
   def items_params
     params.require(:item).permit(:image).merge(:user_id)
   end
->>>>>>> Stashed changes
 end
