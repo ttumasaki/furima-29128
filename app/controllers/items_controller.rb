a5d5cad6858f3dd51c5e4bf539b30647983830ef
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-23 21:40:04 +0900

    バリデーションのまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
+<<<<<<< Updated upstream
   def index
   end
+=======
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:image).merge(:user_id)
+  end
+>>>>>>> Stashed changes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+<<<<<<< Updated upstream
   with_options presence: true do
     validates :nickname
     validates :email, uniqueness: true
@@ -14,6 +15,25 @@ class User < ApplicationRecord
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください。' }
     validates :first_name_kana, format: { with: /\A[ァ-ン]/, message: '全角カタカナのみで入力して下さい' }
     validates :birthday
+=======
+  with_options  presence: true do
+    validates :nickname,
+    validates :email, uniqueness: true
+    validates :password,length: { minimum: 6 },
+                        confirmation: true,
+                        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :password_confirmation
+    validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください。' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ン]/, message: '全角カタカナのみで入力して下さい' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+>>>>>>> Stashed changes
   end
 
   has_many :items
