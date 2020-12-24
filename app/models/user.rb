d28ee822f696700e47452c90432abb42e0da36ec
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-24 15:35:41 +0900

    user.rbのバリデーションまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true,
-                       length: { minimum: 6 },
-                       confirmation: true,
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true
+  with_options  presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :password,length: { minimum: 6 },
+                        confirmation: true,
+                        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :password_confirmation
+    validates :birthday
 
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください。' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]/, message: '全角カタカナのみで入力して下さい' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください。' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]/, message: '全角カタカナのみで入力して下さい' }
-  validates :birthday, presence: true
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください。' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ン]/, message: '全角カタカナのみで入力して下さい' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 
   has_many :items
-  has_many :orders
+  # has_many :orders
 end
