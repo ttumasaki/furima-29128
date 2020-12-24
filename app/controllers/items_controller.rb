74f6912946921a9879c3673627ff426d1d70ffbe
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-24 15:38:50 +0900

    コントローラーにindexとnew、パラメーター用のプライベートアクションを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,23 @@
 class ItemsController < ApplicationController
-  before_action :items_params
+  before_action :items_params,except:[:index]
 
   def index
   end
 
+  def new
+  end
+
   private
   def items_params
-    params.require(:item).permit(:image).merge(:user)
+    binding.pry
+    params.require(:item).permit(:name,
+                                 :explanation,
+                                 :category_id,
+                                 :status_id,
+                                 :fee_burden_id,
+                                 :prefecture_id,
+                                 :day_required_id,
+                                 :price,
+                                 :user)
   end
 end
