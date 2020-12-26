a356684a56d4011318f488a48c161c513928a3fe
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-26 23:21:44 +0900

    カテゴリーの選択肢を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :title, :text, :category, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+  end
 end
