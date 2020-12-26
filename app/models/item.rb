fd5471e5bd1239b18969068554cade875dd30f9f
ttumasaki <be_da_masa@ybb.ne.jp>
2020-12-27 01:13:18 +0900

    アクティブハッシュを使った選択肢実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,37 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-
   validates :title, :text, :category, presence: true
-
   with_options numericality: { other_than: 1 } do
-    validates :category_id
+    validates :category_id, :status_id, :fee_burden
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :status
+  validates :title, :text, :status, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :status_id
+  end
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :fee_burden
+  validates :title, :text, :fee_burden, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :fee_burden_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  validates :title, :text, :prefecture, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :day_required
+  validates :title, :text, :day_required, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :day_required_id
+  end
+
 end
---
+++ spec/factories/day_requireds.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :day_required do
+    
+  end
+end
