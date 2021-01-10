51c4c530f93bcfe4035ac1838137fcfb11719436
ttumasaki <be_da_masa@ybb.ne.jp>
2021-01-10 16:23:31 +0900

    インスタンス変数の修正、投稿の昇順を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: :create
 
   def index
-    @item = Item.all
+    @items = Item.all.order("created_at DEDC")
   end
 
   def new
