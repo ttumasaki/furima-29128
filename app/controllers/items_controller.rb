d279f3b5c610933dc6afd573e68a712716281b51
ttumasaki <be_da_masa@ybb.ne.jp>
2021-01-10 16:31:32 +0900

    綴りの間違い修正、メモを分かりやすく修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: :create
 
   def index
-    @items = Item.all.order("created_at DEDC")
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
