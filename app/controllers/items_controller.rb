adba1b01eb794f4a4ad9dea545ac613d95139591
ttumasaki <be_da_masa@ybb.ne.jp>
2021-01-11 23:35:37 +0900

    出品者だけが商品削除ができる様に設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,9 +37,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if current_user.id == @item.user_id && @item.destroy
       redirect_to root_path
-    end
+    else
+      render :index
+    end 
   end
 
   private
