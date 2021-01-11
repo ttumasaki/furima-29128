3f26d10583b0f0c247eb611256f93eae6188d058
ttumasaki <be_da_masa@ybb.ne.jp>
2021-01-11 22:27:25 +0900

    投稿の削除機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_sign_in, only: [:new]
-  before_action :set_item, only: :create
-  before_action :find_params, only: [:show, :edit, :update]
+  before_action :set_item, only: [:create]
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -37,6 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
