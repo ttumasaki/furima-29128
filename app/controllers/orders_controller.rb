bd33a77e4f078fce9848fc317fb000e389c6a3c0
ttumasaki <be_da_masa@ybb.ne.jp>
2021-01-16 20:44:23 +0900

    商品購入ページの表示設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,18 @@
 class OrdersController < ApplicationController
   def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_address = OrderAddress.new(address_params)
+  end
+
+  private
+
+  def address_params
+    params.require(:order_address).permit(:card_number, :exp_month, :exp_year, :cvc,
+                                         :postal_code, :prefecture_id, :city,
+                                         :address, :building_name, :phone_number)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,7 +3,7 @@ class OrderAddress
   attr_accessor :card_number, :exp_month, :exp_year, :cvc, :postal_code, :prefecture_id, :city,
                 :address, :building_name, :phone_number
 
-  with_option presence: true do
+  with_options presence: true do
     validates :card_number,   format: { with: /\A\d\z/, message: "is invalid.Input half-width number"}
     validates :exp_month,     format: { with: /\A\d\z/, message: "is invalid.Input half-width number"}
     validates :exp_year,      format: { with: /\A\d\z/, message: "is invalid.Input half-width number"}
@@ -15,7 +15,8 @@ class OrderAddress
   end
   
   def save
-    order = Order.create(user)
+    order = Order.create(user: user, item: item)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id:order.id)
   end
 
 end
