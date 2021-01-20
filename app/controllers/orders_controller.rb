201cf917664dcb9ef1f9c6773ecd69db9cd4dd5c
ttumasaki <be_da_masa@ybb.ne.jp>
2021-01-20 19:43:43 +0900

    遷移設定を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,9 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order != nil
+      redirect_to root_path
+    end
   end
 
   def create
