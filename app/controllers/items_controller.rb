b8dff09aa5d669c9cafb49d1ebbbbc5b313c7206
ttumasaki <be_da_masa@ybb.ne.jp>
2021-01-20 22:07:22 +0900

    不要な記述を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_sign_in, only: [:new]
   before_action :set_item, only: [:create]
   before_action :find_params, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
